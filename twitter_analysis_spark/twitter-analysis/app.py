ac07eeb532cbd3e3353cfee5a886137f4e9751eb
Savio Teles <savioteles@gmail.com>
2018-07-18 08:55:56 -0300

    Comentários nos códigos e remoção de códigos desnecessários

--- twitter_analysis_spark/twitter-analysis/app.py
+++ twitter_analysis_spark/twitter-analysis/app.py
@@ -1,10 +1,14 @@
-import json
+# -*- coding: utf-8 -*-
+# Biblioteca cliente do Kafka
 from kafka import SimpleProducer, KafkaClient
+# Biblioteca para consumir os dados da API do Twitter
 import tweepy
+# Biblioteca para ler as configurações de chaves
 import configparser
 
 class TweeterStreamListener(tweepy.StreamListener):
     def __init__(self, api):
+        # Inicia o produtor que irá publicar os tweets no tópico do Kafka
         self.api = api
         super(tweepy.StreamListener, self).__init__()
         client = KafkaClient("10.0.2.15:9092")
@@ -13,8 +17,10 @@ class TweeterStreamListener(tweepy.StreamListener):
                           batch_send_every_t = 10)
 
     def on_status(self, status):
+        # Os tweets chegam nessa função a partir da API do Twitter
         msg =  status.text.encode('utf-8')
         try:
+            # Envia os tweets para o tópico twitterstream do Kafka
             self.producer.send_messages(b'twitterstream', msg)
         except Exception as e:
             print(e)
@@ -22,6 +28,7 @@ class TweeterStreamListener(tweepy.StreamListener):
         return True
 
     def on_error(self, status_code):
+        # Caso ocorra algum erro na coleta do Twitter
         print("Error received in kafka producer")
         return True 
 
@@ -30,6 +37,7 @@ class TweeterStreamListener(tweepy.StreamListener):
 
 if __name__ == '__main__':
 
+    # Lê as chaves de acesso a API do Twitter que estão no arquivo twitter-app-credentials.txt
     config = configparser.ConfigParser()
     config.read('twitter-app-credentials.txt')
     consumer_key = config['DEFAULT']['consumerKey']
@@ -37,21 +45,24 @@ if __name__ == '__main__':
     access_key = config['DEFAULT']['accessToken']
     access_secret = config['DEFAULT']['accessTokenSecret']
 
+    # Cria o cliente do Twitter passando as chaves de acesso
     auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
     auth.set_access_token(access_key, access_secret)
     api = tweepy.API(auth)
 
     while True:
         try:
-            # Connect/reconnect the stream
+            # Conecta/reconecta no stream buscando os tweets com localização e em inglês
             stream = tweepy.Stream(auth, listener = TweeterStreamListener(api))
             stream.filter(locations=[-180,-90,180,90], languages = ['en'], stall_warnings=True)
-        except IncompleteRead:
-            # Reconnect
-            continue
         except KeyboardInterrupt:
-            # Exit this loop
+            print "Bye!"
+            # Se o usuário interromper a execução, termina o loop e disconecta da API do Twitter
             stream.disconnect()
             break
+        except IncompleteRead:
+            # Reconecta
+            continue
+        
 
     
--- twitter_analysis_spark/twitter-analysis/twitterStream.py
+++ twitter_analysis_spark/twitter-analysis/twitterStream.py
@@ -1,24 +1,35 @@
+# -*- coding: utf-8 -*-
+# Importa a biblioteca do spark-kafka
 import os
 os.environ['PYSPARK_SUBMIT_ARGS'] = '--packages org.apache.spark:spark-streaming-kafka_2.10:1.5.1 pyspark-shell'
+# Bibliotecas do Spark Streaming
 from pyspark import SparkConf, SparkContext
 from pyspark.streaming import StreamingContext
+# Biblioteca do Kafka
 from pyspark.streaming.kafka import KafkaUtils
-import operator
 
 def main():
+	# Configura o Spark para rodar localmente com o nome 'Streamer' da aplicação
     conf = SparkConf().setMaster("local[2]").setAppName("Streamer")
     sc = SparkContext(conf=conf)
-
+    # Como o Spark roda em micro batchs, configura o batch em intervalos de 10 segundos
     ssc = StreamingContext(sc, 10)
     ssc.checkpoint("checkpoint")
+
+    # Carrega as palavras positivas do arquivo positive.txt
     pwords = load_wordlist("./Dataset/positive.txt")
+    # Carrega as palavras negativas do arquivo negative.txt
     nwords = load_wordlist("./Dataset/negative.txt")
-    counts = stream(ssc, pwords, nwords, 100)
+    # Chama o método stream local passando o contexto do Spark, os vetores com palavras positivas e negativas e a duração do stream de 30 segundos
+    counts,all_counts = stream(ssc, pwords, nwords, 30)
     print "\n\nAnalise de sentimento no Twitter ao longo do tempo:\n"
+    print "Número total de tweets negativos e positivos: \n"
+    print all_counts[-1]
+    print "\n\nNúmero de tweets positivos e negativos ao longo do tempo: \n"
     print counts
     print "\n\n"
 
-	
+# Esta função lê as palavras de cada linha de um arquivo e retorna em um vetor
 def load_wordlist(filename):
     words = {}
     f = open(filename, 'rU')
@@ -29,47 +40,49 @@ def load_wordlist(filename):
     f.close()
     return words
 
-
-def wordSentiment(word,pwords,nwords):
-    if word in pwords:
-	return ('positive', 1)
-    elif word in nwords:
-	return ('negative', 1)
-
-
+#Esta função é utilizada para somar o total de tweets com sentimento positivo e negativo que já chegaram
 def updateFunction(newValues, runningCount):
     if runningCount is None:
        runningCount = 0
     return sum(newValues, runningCount) 
 
-
-def sendRecord(record):
-    connection = createNewConnection()
-    connection.send(record)
-    connection.close()
-
-
 def stream(ssc, pwords, nwords, duration):
+	# Cria um canal de comunicação com o servidor do Kafka que escuta o tópico twitterstream
     kstream = KafkaUtils.createDirectStream(
     ssc, topics = ['twitterstream'], kafkaParams = {"metadata.broker.list": 'localhost:9092'})
-    tweets = kstream.map(lambda x: x[1].encode("ascii", "ignore"))
 
+    # Cria um stream lê as strings que chegam no tópico do Kafka
+    tweets = kstream.map(lambda x: x[1].encode("ascii", "ignore"))
+    # Para cada tweet que chega, faz um split por espaço para pegar as palavras
     words = tweets.flatMap(lambda line:line.split(" "))
+    # Para cada palavra no tweet verifica se ela está na lista de positivas ou negativas.
+    # Soma 1 se tiver na lista de palavras positivas
     positive = words.map(lambda word: ('Positive', 1) if word in pwords else ('Positive', 0))
+    # Soma 1 se tiver na lista de palavras negativas
     negative = words.map(lambda word: ('Negative', 1) if word in nwords else ('Negative', 0))
+    # Faz uma união dos vetores positivo e negativo
     allSentiments = positive.union(negative)
+    # Faz uma agregação pela chave ('Positive' ou 'Negative') e soma os valores associados
     sentimentCounts = allSentiments.reduceByKey(lambda x,y: x+y)
+    
+    # Ao longo da execução do stream vai salvando e imprimindo na tela o estado do número total de tweets positivos e negativos que já chegaram
     runningSentimentCounts = sentimentCounts.updateStateByKey(updateFunction)
     runningSentimentCounts.pprint()
+    all_counts = []
+    runningSentimentCounts.foreachRDD(lambda t, rdd: all_counts.append(rdd.collect()))
     
+    # Inicializa o vetor que irá guardar a análise de sentimento ao longo do tempo.
     counts = []
+    # Cria o stream que irá rodar a análise de sentimento do Twitter ao longo do tempo
     sentimentCounts.foreachRDD(lambda t, rdd: counts.append(rdd.collect()))
     
+    # Inicia o stream que irá executar por um intervalo de tempo (duration)
     ssc.start() 
     ssc.awaitTerminationOrTimeout(duration)
     ssc.stop(stopGraceFully = True)
 
-    return counts
+    # Retorna o vetor com os resultados
+    return counts,all_counts
 
 
 if __name__=="__main__":

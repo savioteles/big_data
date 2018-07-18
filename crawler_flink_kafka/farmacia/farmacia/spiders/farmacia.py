677c4a46f19243ca5a2807726859bc7a261f1760
Savio Teles <savioteles@gmail.com>
2018-07-18 15:07:58 -0300

    Correção do crawler para funcionar com o Kafka 0.8

--- crawler_flink_kafka/farmacia/farmacia/spiders/farmacia.py
+++ crawler_flink_kafka/farmacia/farmacia/spiders/farmacia.py
@@ -2,12 +2,18 @@
 # biblioteca que vamos utilizar para extrair dados de uma página web
 import scrapy
 # biblioteca cliente do Apache Kafka
-from kafka import KafkaProducer
-# O produtor de mensagens para o Kafka é iniciado apontando para o servidor do Kafka que está escutando localmente na porta 9092
-producer = KafkaProducer(bootstrap_servers='localhost:9092')
+from kafka import SimpleProducer, KafkaClient
 
 class FarmaciaSpider(scrapy.Spider):
-	# nome do spider que é utilizado na inicialização do Spider (scrapy crawl *farmacia*)
+    def __init__(self):
+        super(scrapy.Spider, self).__init__()
+        client = KafkaClient("localhost:9092")
+        # O produtor de mensagens para o Kafka é iniciado apontando para o servidor do Kafka que está escutando localmente na porta 9092
+        self.producer = SimpleProducer(client, async = True,
+                          batch_send_every_n = 1000,
+                          batch_send_every_t = 10)
+
+    # nome do spider que é utilizado na inicialização do Spider (scrapy crawl *farmacia*)
     name = "farmacia"
     # Domínios que serão acessados pelo crawler
     allowed_domains = ["drogaraia.com.br"]
@@ -16,14 +22,14 @@ class FarmaciaSpider(scrapy.Spider):
     )
 
     def parse(self, response):
-    	# Para cada página web de resposta devemos fazer o parser. Vamos buscar as informações dos medicamentos e olhando para o HTML estas informações vem nas divs com class product-info
+        # Para cada página web de resposta devemos fazer o parser. Vamos buscar as informações dos medicamentos e olhando para o HTML estas informações vem nas divs com class product-info
         medicamentos = response.xpath('//div[@class="product-info"]')
         for medicamento in medicamentos:
-        	# Como são vários médicamentos com a class="product-info", percorremos cada um buscando seus atributos. O primeiro é o nome do medicamento que no HTML está na class="product-name" no atributo title de <a>
-        	nome_medicamento = medicamento.xpath('div[@class="product-name"]/a/@title').extract_first().encode('utf-8')
-        	if nome_medicamento is not None:
-        		print "Nome do medicamento: " +nome_medicamento
-        		# Agora busca os detalhes dos medicamentos que serão utilizados como dados a serem enviados para o Kafka
+            # Como são vários médicamentos com a class="product-info", percorremos cada um buscando seus atributos. O primeiro é o nome do medicamento que no HTML está na class="product-name" no atributo title de <a>
+            nome_medicamento = medicamento.xpath('div[@class="product-name"]/a/@title').extract_first().encode('utf-8')
+            if nome_medicamento is not None:
+                print "Nome do medicamento: " +nome_medicamento
+                # Agora busca os detalhes dos medicamentos que serão utilizados como dados a serem enviados para o Kafka
                 detalhes_medicamento = medicamento.xpath('div[@class="product-attributes"]')
                 # A marca do medicamento está no texto HTML na class "marca". 
                 # Repare que aqui estamos utilizando o contains para encontrar a class, ou seja, 
@@ -43,16 +49,13 @@ class FarmaciaSpider(scrapy.Spider):
                 dados_medicamento = nome_medicamento +";" + marca +";" +quantidade +";" +principio_ativo +";" +preco_atual
                 print "Dados do medicamento: " +dados_medicamento
                 # Enviamos a mensagem para o tópico farmacia. Todos que estiverem escutando neste tópico do Kafka irão receber a mensagem.
-                producer.send('farmacia', dados_medicamento)
-        # Se tiver alguma mensagem para ser enviada fica parado neste ponto abaixo de flush até que estas mensagens sejam enviadas.
-        producer.flush()
-
+                self.producer.send_messages(b'farmacia', dados_medicamento)
         # Obtém o link da próxima página buscando a url que está apontando o botão "Próximo" na página HTML
         next_page = response.xpath('//li[contains(@class, "current inline")]/text()').extract_first()
         url_np = response.xpath('//a[contains(@class, "next i-next btn-more")]/@href').extract_first(default='not-found').encode('utf-8').strip()
         
         if url_np:
-        	# Inicia o crawler da próxima página e retorna.
+            # Inicia o crawler da próxima página e retorna.
             cookie = response.headers
             request = scrapy.Request(url=url_np, headers={'Cookie': cookie}, callback=self.parse)
             yield request
--- crawler_flink_kafka/install.sh
+++ crawler_flink_kafka/install.sh
@@ -2,5 +2,4 @@
 sudo apt-get update
 sudo apt-get install -y python-pip
 sudo pip install --upgrade pip
-sudo pip install scrapy
-sudo pip install kafka-python
+sudo pip install -r farmacia/requirements.txt
